e26ba8c1255f839c8c0a3e43ed9a86ba2c3fbe33
Alan Varela <alanvareli@gmail.com>
2024-10-16 10:26:59 -0600

    Update practicaHanoi.asm

--- practicaHanoi.asm
+++ practicaHanoi.asm
@@ -1,2 +1,7 @@
-//Torres de Hanoi 
-//Alan David Varela Maldonado
+#Practica 1: Torres de Hanoi
+#Fecha: 24 - Oct - 2024
+#Autor: Alan David Varela Maldonado
+#Descripcion: Resolver el problema de las torres de Hanoi
+#             utilizando un algoritmo recursivo en RISC V
+
+
